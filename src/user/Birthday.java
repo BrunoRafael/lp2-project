210c7b0435127b0aad7a950ad76e310cacb18515
Andre Leite <andre.luiz.leite@ccc.ufcg.edu.br>
2012-08-30 07:17:05 -0300

    Adds documentation on Birthday class.

--- src/user/Birthday.java
+++ src/user/Birthday.java
@@ -2,14 +2,32 @@ package user;
 
 import java.util.GregorianCalendar;
 
+/**
+ * Class representing a Birthday.
+ * @author andreluiz
+ *
+ */
 public class Birthday {
 	
+	/**
+	 * Attribute that will hold the actual date.
+	 */
 	private GregorianCalendar date;
 
+	/**
+	 * Default constructor of Birthday class.
+	 * @param day day of birth.
+	 * @param month month of birth.
+	 * @param year year of birth.
+	 */
 	public Birthday(int day, int month, int year) {
 		this.setDate(day, month, year);
 	}
 	
+	/**
+	 * 
+	 * @return Date of birth represented by a GregorianCalendar object.
+	 */
 	public GregorianCalendar getDate() {
 		return date;
 	}
@@ -19,6 +37,12 @@ public class Birthday {
 		return "Birthday [date=" + date + "]";
 	}
 
+	/**
+	 * Sets the date attribute to a new GregorianCalendar object with the given parameters.
+	 * @param day day of birth of the new object.
+	 * @param month month of birth of the new object.
+	 * @param year year of birth of the new object.
+	 */
 	public void setDate(int day, int month, int year) {
 		this.date = new GregorianCalendar(year, month, day);
 	}
