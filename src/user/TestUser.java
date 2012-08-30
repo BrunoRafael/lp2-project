656eb2ef7dbb5da92ca7096d3ba5b07f8ca8c077
Andre Leite <andre.luiz.leite@ccc.ufcg.edu.br>
2012-08-30 07:40:09 -0300

    Initial work on TestUser.

---
+++ src/user/TestUser.java
@@ -0,0 +1,61 @@
+package user;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class TestUser extends TestCase {
+
+	private User u;
+	private User u1;
+	
+	@Test
+	public void testUser() {
+		u = new User("fulano@ccc.com", "root00", 24, 8, 1989);
+		u1 = new User("cicrano@ddd.com", "root11", 30, 8, 1989);
+		
+		assertEquals("fulano", u.getEmail().getLocal());
+		assertEquals("ccc.com", u.getEmail().getDomain());
+		assertFalse(u.equals(u1));
+		User u2 = new User(u);
+		assertEquals(u, u2);
+		
+	}
+
+	@Test
+	public void testGetPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBirthday() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBirthday() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
