9e9288f1920afc2815df905889317718c685712b
Andre Leite <andre.luiz.leite@ccc.ufcg.edu.br>
2012-08-30 06:41:33 -0300

    Initial work on Password test class.

---
+++ src/user/PasswordTest.java
@@ -0,0 +1,47 @@
+package user;
+
+import static org.junit.Assert.*;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.NoSuchPaddingException;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class PasswordTest extends TestCase {
+
+	private String firstPass = "root00";
+	
+	@Test
+	public void testPassword() throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException {
+		Password p = new Password(firstPass);
+		Password p1 = new Password("root11");
+		assertTrue(Password.comparePasswords(p, firstPass));
+		assertFalse(Password.comparePasswords(p1, firstPass));
+		assertTrue(Password.comparePasswords(p1, "root11"));
+	}
+
+	@Test
+	public void testGetKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEncryptedPass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetEncryptedPass() {
+		fail("Not yet implemented");
+	}
+
+}
