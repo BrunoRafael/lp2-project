28721a4f81436a49e3d330c4c3ebf68877685bc9
Andre Leite <andre.luiz.leite@ccc.ufcg.edu.br>
2012-08-30 07:36:43 -0300

    Added equals method on Birthday.

--- src/user/Birthday.java
+++ src/user/Birthday.java
@@ -47,4 +47,28 @@ public class Birthday {
 		this.date = new GregorianCalendar(year, month, day);
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Birthday other = (Birthday) obj;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		return result;
+	}
 }
