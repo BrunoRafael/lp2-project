dd168c72ca34d2c040e0f9dda25c8211f164c622
Andre Leite <andre.luiz.leite@ccc.ufcg.edu.br>
2012-08-30 07:21:38 -0300

    Adds documentation on User class.

--- src/user/User.java
+++ src/user/User.java
@@ -1,11 +1,35 @@
 package user;
 
+/**
+ * Class representing an user.
+ * @author andreluiz
+ *
+ */
 public class User {
 	
+	/**
+	 * Password object of the User instance.
+	 */
 	private Password password;
+	
+	/**
+	 * Email object of the User instance.
+	 */
 	private Email email;
+	
+	/**
+	 * Birthday object of the User instance.
+	 */
 	private Birthday birthday;
 	
+	/**
+	 * Constructor of an User instance.
+	 * @param email Email of the user. This will be used as a login.
+	 * @param password Password of the user. This will be encrypted.
+	 * @param day Day of birth of the user.
+	 * @param month Month of birth of the user.
+	 * @param year Yearh of birth of the user.
+	 */
 	public User(String email, String password,
 			int day, int month, int year) {
 		this.setPassword(new Password(password));
@@ -13,26 +37,51 @@ public class User {
 		this.setBirthday(new Birthday(day, month, year));
 
 	}
+	
+	/**
+	 * 
+	 * @return Password object of the User instance.
+	 */
 	public Password getPassword() {
 		return password;
 	}
 	
+	/**
+	 * 
+	 * @param password Password object to set the on User instance. 
+	 */
 	public void setPassword(Password password) {
 		this.password = password;
 	}
 	
+	/**
+	 * 
+	 * @return Email object of the User instance.
+	 */
 	public Email getEmail() {
 		return email;
 	}
 	
+	/**
+	 * 
+	 * @param email Email object to set the on User instance.
+	 */
 	public void setEmail(Email email) {
 		this.email = email;
 	}
 	
+	/**
+	 * 
+	 * @return Birthday of the User instance.
+	 */
 	public Birthday getBirthday() {
 		return birthday;
 	}
 	
+	/**
+	 * 
+	 * @param birthday Birthday object to set the on User instance.
+	 */
 	public void setBirthday(Birthday birthday) {
 		this.birthday = birthday;
 	}
