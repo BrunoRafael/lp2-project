00a3253b73f3664c016fe7375548cd02d8beacac
Andre Leite <andre.luiz.leite@ccc.ufcg.edu.br>
2012-08-30 07:38:43 -0300

    Adds equals method on User class.

--- src/user/User.java
+++ src/user/User.java
@@ -101,6 +101,45 @@ public class User {
 		return "User [password=" + password + ", email=" + email
 				+ ", birthday=" + birthday + "]";
 	}
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((birthday == null) ? 0 : birthday.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result
+				+ ((password == null) ? 0 : password.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (birthday == null) {
+			if (other.birthday != null)
+				return false;
+		} else if (!birthday.equals(other.birthday))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+			else
+				return true;
+		}
+		return true;
+	}
 
 }
