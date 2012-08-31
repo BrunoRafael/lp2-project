2b703713928fe6edd2f007394365e920ba7fe72b
Andre Leite <andre.luiz.leite@ccc.ufcg.edu.br>
2012-08-31 10:33:36 -0300

    Adds documentation on PersistentUser class.

--- src/persistency/PresistentUser.java
+++ src/persistency/PresistentUser.java
@@ -4,6 +4,11 @@ import java.io.Serializable;
 
 import user.User;
 
+/**
+ * Class that makes User serializable.
+ * @author andreluiz
+ *
+ */
 public class PresistentUser extends User implements Serializable {
 
 	/**
@@ -12,11 +17,23 @@ public class PresistentUser extends User implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Complete constructor of PersistentUser. This will create a User and persist it.
+	 * @param email Email to be used.
+	 * @param password Password to be used.
+	 * @param day Day of birth.
+	 * @param month Month of birth.
+	 * @param year Year of birth.
+	 */
 	public PresistentUser(String email, String password, int day, int month,
 			int year) {
 		super(email, password, day, month, year);
 	}
 
+	/**
+	 * Constructor of PersistentUser using a User instance.
+	 * @param newUser User to Persisted.
+	 */
 	public PresistentUser(User newUser) {
 		super(newUser);
 	}
