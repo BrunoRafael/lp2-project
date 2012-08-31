bcea781eb7b211d76fec1e21e4580c9d8c74e536
Andre Leite <andre.luiz.leite@ccc.ufcg.edu.br>
2012-08-31 07:56:45 -0300

    Adding documentation on PlaylistMusic class.

--- src/music/PlaylistMusic.java
+++ src/music/PlaylistMusic.java
@@ -7,20 +7,47 @@ import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
 import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
 import org.jaudiotagger.tag.TagException;
 
+/**
+ * Class that represents a Music object in a playlist.
+ * 
+ * @author andreluiz
+ *
+ */
 public class PlaylistMusic extends Music {
 
+	/**
+	 * Qualification attribute of a music in a playlist.
+	 * The default value is 0.
+	 */
 	private int qualification;
 	
+	/**
+	 * Constructor of a PlaylistMusic object.
+	 * @param path Path to the music object.
+	 * @throws CannotReadException JAudioTagger related.
+	 * @throws IOException 
+	 * @throws TagException JAudioTagger related.
+	 * @throws ReadOnlyFileException JAudioTagger related.
+	 * @throws InvalidAudioFrameException JAudioTagger related.
+	 */
 	public PlaylistMusic(String path) throws CannotReadException, IOException,
 			TagException, ReadOnlyFileException, InvalidAudioFrameException {
 		super(path);
 		this.setQualification(0);
 	}
 
+	/**
+	 * 
+	 * @return Current qualification score.
+	 */
 	public int getQualification() {
 		return qualification;
 	}
 
+	/**
+	 * The qualification should be an integer and range from 0 to 5.
+	 * @param qualification
+	 */
 	public void setQualification(int qualification) {
 		this.qualification = (qualification > 0 && qualification < 5) ? qualification:
 																		0;
