855649c6805e41ffcabf5a424c2ef592d5910557
Andre Leite <andre.luiz.leite@ccc.ufcg.edu.br>
2012-09-02 17:38:23 -0300

    Initial test on PersistencyHandlerTest class.

--- src/persistency/PersistencyHandlerTest.java
+++ src/persistency/PersistencyHandlerTest.java
@@ -2,13 +2,44 @@ package persistency;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
+import org.jaudiotagger.audio.exceptions.CannotReadException;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.tag.TagException;
 import org.junit.Test;
 
 public class PersistencyHandlerTest {
 
 	@Test
 	public void testPersistencyHandler() {
-		fail("Not yet implemented");
+		PersistencyHandler ph = PersistencyHandler.getInstance();
+		ph.loadDb();
+		try {
+			ph.write(new PersistentMusic("/home/andreluiz/a.mp3"));
+			ph.write(new PersistentMusic("/home/andreluiz/b.mp3"));
+			ph.write(new PersistentUser("andrelgsl@gmail.com",
+										"1234", 24, 8, 1989));				
+		} catch (CannotReadException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TagException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ReadOnlyFileException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidAudioFrameException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ph.readMusicBuffer();
+		ph.readUserBuffer();
+		ph.saveAll();
 	}
 
 	@Test
