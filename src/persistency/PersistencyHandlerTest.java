fa60c2ccbabd96740ac523eded417fc5aefbf34d
Andre Leite <andre.luiz.leite@ccc.ufcg.edu.br>
2012-09-02 16:56:48 -0300

    Initial work on the PersistencyHandlerTest class.

---
+++ src/persistency/PersistencyHandlerTest.java
@@ -0,0 +1,139 @@
+package persistency;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PersistencyHandlerTest {
+
+	@Test
+	public void testPersistencyHandler() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetInstance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaveAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoadDb() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWritePersistentUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWritePersistentMusic() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetuserDbOut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUserDbOut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetuserFileOut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetuserFileOut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserFileIn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUserFileIn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserDbIn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUserDbIn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMusicFileOut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMusicFileOut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMusicFileIn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMusicFileIn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMusicDbOut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMusicDbOut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMusicDbIn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMusicDbIn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserBuffer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUserBuffer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMusicBuffer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMusicBuffer() {
+		fail("Not yet implemented");
+	}
+
+}
